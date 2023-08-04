30b006f1c0c5e2f0a564ffd3695ace7ccea13692
lopezrsol <mariasollopez23@gmail.com>
2023-08-04 13:20:51 -0300

    clase 17, diseño basico nosotros, carrito

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
+import {resolve} from 'node:path'
+
 export default {
   css: {
     devSourcemap: true,
   },
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        carrito: resolve('pages/carrito.html'),
+        contacto: resolve('pages/contacto.html'),
+        nosotros: resolve('pages/nosotros.html'),
+        inicio: resolve('index.html')
+        
+      }
+    }
+     
+  }
 };
