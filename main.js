243b2e7f631a5cbfbfb732a2d20b0c7415f7b35a
lopezrsol <mariasollopez23@gmail.com>
2023-08-25 18:08:45 -0300

    se agrega nosotros, contacto, se modifica footer y header

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
-
+import './css/custom.css'
 import './css/style.css'
